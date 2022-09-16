b1a68a2037d7bab7a2cb228bc92eeefd612036c9
Grace Effiong <graceffiong@gmail.com>
2022-09-16 11:05:04 +0000

    task 0 and 1

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -4,5 +4,6 @@ file { '0-create_a_file':
   path    => '/tmp/school',
   owner   => 'www-data',
   group   => 'www-data',
+  mode    => '0744',
   content => 'I love Puppet',
 }
--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,3 +1,6 @@
 # using puppet, install flask from pip3
- include python
-include python::flask
+
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
