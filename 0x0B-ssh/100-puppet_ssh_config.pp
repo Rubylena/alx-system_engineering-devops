3f1460d1040c47a2183726282b722d9253bc66cf
Grace Effiong <graceffiong@gmail.com>
2022-09-18 19:44:32 +0000

    task 4

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,5 @@
 # make changes to config file using puppet
-file_line {'Turn off password auth':
+file_line {'Turn off passwd auth':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => 'PasswordAuthentication no',
