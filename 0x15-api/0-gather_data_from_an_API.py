e62bf9526e76a09304e5545c5c223927fd74fd87
Ruby <graceffiong@gmail.com>
2022-10-21 22:02:25 +0100

    pycodestyle

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,14 +6,16 @@ from sys import argv
 
 def userTodo():
     id = int(argv[1])
+    api = "https://jsonplaceholder.typicode.com/"
     if (len(argv) > 1):
-        reqTodo = requests.get("https://jsonplaceholder.typicode.com/todos/").json()
+        reqTodo = requests.get('{}todos/'.format(api)).json()
         filtered = list(filter(lambda x: x.get('userId') == id, reqTodo))
         todoComplete = list(filter(lambda x: x.get('completed'), filtered))
 
-        reqUser = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(id)).json()
+        reqUser = requests.get('{}users/{}'.format(api, id)).json()
         name = reqUser.get('name')
-        print('Employee {} is done with tasks({}/{}):'.format(name, len(todoComplete), len(filtered)))
+        print('Employee {} is done with tasks({}/{}):'
+              .format(name, len(todoComplete), len(filtered)))
         for i in todoComplete:
             print('\t{}'.format(i.get('title')))
 
