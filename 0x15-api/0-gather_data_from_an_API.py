59ecd08e0b2d24d025fde8576238e35784cc3762
Ruby <graceffiong@gmail.com>
2022-10-21 13:46:40 +0100

    function should not be imported

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,16 +3,20 @@
 import requests
 from sys import argv
 
-id = int(argv[1])
 
-reqTodo = requests.get("https://jsonplaceholder.typicode.com/todos/")
-jsonTo = reqTodo.json()
-filtered = list(filter(lambda x: x.get('userId') == id, jsonTo))
-todoComplete = list(filter(lambda x: x.get('completed'), filtered))
+def userTodo():
+    id = int(argv[1])
+    if (len(argv) > 1):
+        reqTodo = requests.get("https://jsonplaceholder.typicode.com/todos/").json()
+        filtered = list(filter(lambda x: x.get('userId') == id, reqTodo))
+        todoComplete = list(filter(lambda x: x.get('completed'), filtered))
 
-reqUser = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(id))
-json = reqUser.json()
-name = json.get('name')
-print('Employee {} is done with tasks({}/{}):'.format(name, len(todoComplete), len(filtered)))
-for i in todoComplete:
-    print('\t{}'.format(i.get('title')))
+        reqUser = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(id)).json()
+        name = reqUser.get('name')
+        print('Employee {} is done with tasks({}/{}):'.format(name, len(todoComplete), len(filtered)))
+        for i in todoComplete:
+            print('\t{}'.format(i.get('title')))
+
+
+if __name__ == '__main__':
+    userTodo()
