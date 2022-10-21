23422b1bdd3145e37c9b63549b1a84af97f11719
Ruby <graceffiong@gmail.com>
2022-10-21 22:15:46 +0100

    add space after tab

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,19 +5,19 @@ from sys import argv
 
 
 def userTodo():
-    id = int(argv[1])
     api = "https://jsonplaceholder.typicode.com/"
     if (len(argv) > 1):
-        reqTodo = requests.get('{}todos/'.format(api)).json()
+        id = int(argv[1])
+        reqUser = requests.get('{}users/{}'.format(api, id)).json()
+        reqTodo = requests.get('{}todos'.format(api)).json()
         filtered = list(filter(lambda x: x.get('userId') == id, reqTodo))
         todoComplete = list(filter(lambda x: x.get('completed'), filtered))
 
-        reqUser = requests.get('{}users/{}'.format(api, id)).json()
         name = reqUser.get('name')
         print('Employee {} is done with tasks({}/{}):'
               .format(name, len(todoComplete), len(filtered)))
         for i in todoComplete:
-            print('\t{}'.format(i.get('title')))
+            print('\t {}'.format(i.get('title')))
 
 
 if __name__ == '__main__':
