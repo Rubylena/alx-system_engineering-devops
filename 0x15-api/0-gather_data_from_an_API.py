77bd4fc7d0097c78b970515e38657e20356a7ac9
Ruby <graceffiong@gmail.com>
2022-10-21 22:32:59 +0100

    formatter

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,16 +5,17 @@ from sys import argv
 
 
 def userTodo():
+    """ returns information about his/her TODO list progress. """
     api = "https://jsonplaceholder.typicode.com/"
     if (len(argv) > 1):
         id = int(argv[1])
-        reqUser = requests.get('{}users/{}'.format(api, id)).json()
+        reqUser = requests.get('{}users/{:d}'.format(api, id)).json()
         reqTodo = requests.get('{}todos'.format(api)).json()
         filtered = list(filter(lambda x: x.get('userId') == id, reqTodo))
         todoComplete = list(filter(lambda x: x.get('completed'), filtered))
 
         name = reqUser.get('name')
-        print('Employee {} is done with tasks({}/{}):'
+        print('Employee {} is done with tasks({:d}/{:d}):'
               .format(name, len(todoComplete), len(filtered)))
         for i in todoComplete:
             print('\t {}'.format(i.get('title')))
