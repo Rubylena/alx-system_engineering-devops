7afd4fed23fc1f464f78cc1ef1fb19eacbeb3a24
Ruby <graceffiong@gmail.com>
2022-10-21 13:33:41 +0100

    task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Import request module """
+import requests
+from sys import argv
+
+id = int(argv[1])
+
+reqTodo = requests.get("https://jsonplaceholder.typicode.com/todos/")
+jsonTo = reqTodo.json()
+filtered = list(filter(lambda x: x.get('userId') == id, jsonTo))
+todoComplete = list(filter(lambda x: x.get('completed'), filtered))
+
+reqUser = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(id))
+json = reqUser.json()
+name = json.get('name')
+print('Employee {} is done with tasks({}/{}):'.format(name, len(todoComplete), len(filtered)))
+for i in range(len(todoComplete)):
+    print('\t{}'.format(todoComplete[i]['title']))
---
+++ 0x15-api/README.md
@@ -0,0 +1,8 @@
+## Tasks
+* 0. Write a Python script that, using this REST API, for a given employee ID, returns information about his/her TODO list progress.
+
+* 1. Using what you did in the task #0, extend your Python script to export data in the CSV format.
+
+* 2. Using what you did in the task #0, extend your Python script to export data in the JSON format, for a user.
+
+* 3. Using what you did in the task #0, extend your Python script to export data in the JSON format, for all employees.
